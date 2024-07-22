54a3057cb387eb967ed90e91ad4448827d57f93f
donayel <a.yelouassi@gmail.com>
2024-07-22 16:24:35 +0100

    Commentaire sur le code du fichier Electeur.cs

--- ElecteurAPI/Models/Electeur.cs
+++ ElecteurAPI/Models/Electeur.cs
@@ -4,12 +4,19 @@ namespace ElecteurAPI.Models
 {
     public class Electeur
     {
+        // Id auto incrément de l'électeur
         public int Id { get; set; }
+        // Numéro de l'électeur
         public string NumeroElecteur { get; set; }
+        // Nom de l'électeur
         public string Nom { get; set; }
+        // Prénom de l'électeur
         public string Prenoms { get; set; }
+        // Date de naissance de l'électeur 
         public DateTime DateNaissance { get; set; }
+        //Contact de l'électeur
         public string Contact { get; set; }
+        // Email de l'électeur
         public string Email { get; set; }
     }
 }
--- ElecteurAPI/obj/Debug/net8.0/ElecteurAPI.AssemblyInfo.cs
+++ ElecteurAPI/obj/Debug/net8.0/ElecteurAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElecteurAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52e5d238462f50d30968b9214b7d34906ed65b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElecteurAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElecteurAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
